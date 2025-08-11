2360788176167b6d5cd955a6e0c14f1b02ba6ee9
Marcio Garozzo <mgarozzo@frba.utn.edu.ar>
2025-08-11 02:43:18 -0300

    feat: add role to active admin page

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register User do
-  permit_params :email, :first_name, :last_name, :username, :password, :password_confirmation
+  permit_params :email, :first_name, :last_name, :username, :password, :password_confirmation, :role
 
   if ENV['IMPERSONATION_URL'].present?
     member_action :impersonate, method: :post do
@@ -18,6 +18,7 @@ ActiveAdmin.register User do
       f.input :first_name
       f.input :last_name
       f.input :username
+      f.input :role, as: :select, collection: User.roles.keys.map { |key| [key.humanize, key] }
 
       if f.object.new_record?
         f.input :password
@@ -35,6 +36,9 @@ ActiveAdmin.register User do
     column :first_name
     column :last_name
     column :username
+    column :role do |user|
+      status_tag user.role.humanize, class: user.admin? ? 'important' : 'ok'
+    end
     column :sign_in_count
     column :created_at
     column :updated_at
@@ -47,6 +51,7 @@ ActiveAdmin.register User do
   filter :username
   filter :first_name
   filter :last_name
+  filter :role, as: :select, collection: User.roles.keys.map { |key| [key.humanize, key] }
   filter :created_at
   filter :updated_at
 
@@ -57,6 +62,9 @@ ActiveAdmin.register User do
       row :first_name
       row :last_name
       row :username
+      row :role do |user|
+        status_tag user.role.humanize, class: user.admin? ? 'important' : 'ok'
+      end
       row :sign_in_count
       row :created_at
       row :updated_at
