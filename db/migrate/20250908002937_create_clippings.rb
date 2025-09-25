db09940e18ddf9a21ffaa2905d1a65665545021b
Marcio Garozzo <marcio.garozzo@widergy.com>
2025-09-25 02:17:48 -0300

    feat: add indexes to clipping model

--- db/migrate/20250908002937_create_clippings.rb
+++ db/migrate/20250908002937_create_clippings.rb
@@ -7,7 +7,7 @@ class CreateClippings < ActiveRecord::Migration[8.0]
       t.date :period_start, null: false
       t.date :period_end, null: false
       t.jsonb :filters, null: false, default: {}
-      t.jsonb :news_ids, null: false, default: [], array: true
+      t.jsonb :news_ids, null: false, default: []
       t.references :creator, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
@@ -15,5 +15,7 @@ class CreateClippings < ActiveRecord::Migration[8.0]
 
     add_index :clippings, :period_start
     add_index :clippings, :period_end
+    add_index :clippings, :filters, using: :gin
+    add_index :clippings, :news_ids, using: :gin
   end
 end
