d0161eed23073cf29a8bb7c2cc421bbb02e9579d
Marcio Garozzo <marcio.garozzo@widergy.com>
2025-09-25 23:20:40 -0300

    test: add routing specs

--- spec/routing/sessions_routing_spec.rb
+++ spec/routing/sessions_routing_spec.rb
@@ -3,11 +3,11 @@
 describe API::V1::SessionsController do
   describe 'routing' do
     it 'routes to #create' do
-      expect(post: '/api/v1/users/sign_in').to route_to('api/v1/sessions#create')
+      expect(post: '/api/v1/users/sign_in').to route_to('api/v1/sessions#create', format: :json)
     end
 
     it 'routes to #destroy' do
-      expect(delete: 'api/v1/users/sign_out').to route_to('api/v1/sessions#destroy')
+      expect(delete: '/api/v1/users/sign_out').to route_to('api/v1/sessions#destroy', format: :json)
     end
   end
 end
