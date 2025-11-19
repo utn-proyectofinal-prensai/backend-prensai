89ee1f908c638d4b5f9c143426c550426ebb7a41
Marcio Garozzo <marcio.garozzo@widergy.com>
2025-11-18 21:26:40 -0300

    fix: quotation limit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_27_053201) do
+ActiveRecord::Schema[8.0].define(version: 2025_11_19_002207) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -270,7 +270,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_27_053201) do
     t.string "interviewee"
     t.string "link", null: false
     t.integer "audience_size"
-    t.decimal "quotation", precision: 10, scale: 2, default: "0.0"
+    t.decimal "quotation", precision: 15, scale: 2, default: "0.0"
     t.string "valuation"
     t.string "political_factor"
     t.text "plain_text"
